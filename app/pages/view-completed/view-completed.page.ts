bf7d8eba071c1b8f28405fee8a720c91a8df2aed
Tim <tja2353@gmail.com>
2016-09-04 15:11:34 +0100

    Edit ViewCompleletedPage

--- app/pages/view-completed/view-completed.page.ts
+++ app/pages/view-completed/view-completed.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 
 import { TodoCompletedService } from '../../services/todo-completed.service';
@@ -10,6 +10,9 @@ import {
   TodoCompletedListComponent
 } from '../../components/todo-completed-list/todo-completed-list.component';
 
+import { TodoCompleted } from '../../models/todo-completed';
+import { TodoCompletedPage } from '../todo-completed/todo-completed.page';
+
 @Component({
   directives: [TodoCompletedListComponent],
   templateUrl: 'build/pages/view-completed/view-completed.page.html'
@@ -19,6 +22,7 @@ export class ViewCompletedPage {
 
   constructor(
     public navCtrl: NavController,
+    public modalCtrl: ModalController,
     public todoCompletedService: TodoCompletedService
   ) {
     //
@@ -31,5 +35,16 @@ export class ViewCompletedPage {
 
   editItem(item: EditItemOutput) {
     console.log('editItem:item>', item);
+    let modal = this.modalCtrl.create(TodoCompletedPage, { todo: item });
+
+    modal.onDidDismiss((data: TodoCompleted) => {
+      console.log('onDidDismiss>', data);
+
+      if (!!data) {
+        this.todoCompletedService.save(data);
+      }
+    });
+
+    modal.present();
   }
 }
